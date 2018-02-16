0ba470d510e982505a846dce548e278544215f0b
Chris Cornutt <enygma@phpdeveloper.org>
2018-02-16 08:11:17 -0600

    adding first tests for Instance class

--- tests/InstanceTest.php
+++ tests/InstanceTest.php
@@ -2,6 +2,8 @@
 
 use \Psecio\Canary\Instance;
 use \Psecio\Canary\Data;
+use \Psecio\Canary\Criteria;
+use \Psecio\Canary\CriteriaSet;
 
 class InstanceTest extends \PHPUnit\Framework\TestCase
 {
@@ -41,4 +43,38 @@ class InstanceTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($data, $instance->getData());
     }
 
+    public function testBuildStatic()
+    {
+        $config = [
+            'data' => ['test' => true]
+        ];
+        $i = Instance::build($config);
+
+        $this->assertInstanceOf(Instance::class, $i);
+
+        // The config should match
+        $this->assertEquals($config, $i->getConfig());
+    }
+
+    public function testAddCriteriaSimple()
+    {
+        $i = Instance::build();
+        $i->if('username', 'test');
+        $criteria = $i->getCriteria();
+
+        $this->assertInstanceOf(CriteriaSet::class, $criteria);
+        $this->assertCount(1, $criteria);
+    }
+
+    public function testAddCriteriaSet()
+    {
+        $set = new CriteriaSet();
+        $cinstance = new \Psecio\Canary\Criteria\Equals('username', 'test');
+        $set->add($cinstance);
+        
+        $i = Instance::build();
+        $i->if($set);
+
+        $this->assertEquals($set, $i->getCriteria());
+    }
 }
