a126ba7e3db8649102bd911d4160df97d8fc8603
Chris Cornutt <enygma@phpdeveloper.org>
2018-02-17 10:35:52 -0600

    removing debugging from the ErrorLog notifier

--- src/Notify/ErrorLog.php
+++ src/Notify/ErrorLog.php
@@ -8,7 +8,6 @@ class ErrorLog extends \Psecio\Canary\Notify
 {
     public function execute(Criteria $criteria)
     {
-        echo __CLASS__.' :: '.__FUNCTION__."\n";
         error_log('Canary match: '.json_encode($criteria));
     }
 }
