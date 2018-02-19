c4c23f197c0728427922706c735ee686aaffd9e5
Chris Cornutt <enygma@phpdeveloper.org>
2018-02-19 15:05:24 -0600

    adding more tests for the Instance class

--- tests/InstanceTest.php
+++ tests/InstanceTest.php
@@ -4,6 +4,7 @@ use \Psecio\Canary\Instance;
 use \Psecio\Canary\Data;
 use \Psecio\Canary\Criteria;
 use \Psecio\Canary\CriteriaSet;
+use \Psecio\Canary\Criteria\Equals;
 
 class InstanceTest extends \PHPUnit\Framework\TestCase
 {
@@ -71,10 +72,87 @@ class InstanceTest extends \PHPUnit\Framework\TestCase
         $set = new CriteriaSet();
         $cinstance = new \Psecio\Canary\Criteria\Equals('username', 'test');
         $set->add($cinstance);
-        
+
         $i = Instance::build();
         $i->if($set);
 
         $this->assertEquals($set, $i->getCriteria());
+    }
+
+    public function testAddSingleCriteria()
+    {
+        $criteria = new Equals('test', 'foo');
+        $i = Instance::build()->if($criteria);
+
+        $set = $i->getCriteria();
+        $this->assertCount(1, $set);
+
+        $c = $set[0];
+        $this->assertEquals($c, $criteria);
+    }
+
+    public function testAddMultipleKeyValue()
+    {
+        $criteria = [
+            'foo' => 'bar',
+            'baz' => 'quux'
+        ];
+        $i = Instance::build()->if($criteria);
+        $set = $i->getCriteria();
+
+        // The values are stored in a set so this set is the single value
+        $this->assertCount(1, $set);
+        $this->assertCount(2, $set[0]);
+
+        $set = $set[0];
+        // Ensure that our values are the same as what we gave
+        $first = $set[0]->toArray();
+        $this->assertEquals('bar', $first['value']);
+
+        $second = $set[1]->toArray();
+        $this->assertEquals('quux', $second['value']);
+    }
+
+    public function testAddNotifyToLast()
+    {
+        $callback = function() { echo 'test'; };
+        $config = [ 'callback' => $callback ];
+
+        $notify = new \Psecio\Canary\Notify\Callback($config);
+        $i = Instance::build()->if(['test' => 'foo'])->then($notify);
+
+        $set = $i->getCriteria();
+        $criteria = $set[0][0];
+
+        $notify = $criteria->getNotify();
+        // print_r($notify);
+        $this->assertInstanceOf(\Psecio\Canary\Notify\Callback::class, $notify);
+        $this->assertEquals($notify->getConfig('callback'), $callback);
+    }
+
+    public function testResolveNotify()
+    {
+        $notify = new \Psecio\Canary\Notify\Callback(['callback' => function() {}]);
+        $i = Instance::build();
+
+        $result = $i->resolveNotify($notify);
+        $this->assertEquals($result, $notify);
+
+        // Now try with a callback
+        $result = $i->resolveNotify(function() { });
+        $this->assertInstanceOf(\Psecio\Canary\Notify\Callback::class, $result);
+    }
+
+    /**
+     * Throws an exception on a bad notify type
+     *
+     * @expectedException \InvalidArgumentException
+     */
+    public function testResolveNotifyInvalid()
+    {
+        $notify = (object)['foo' => 'badvalue'];
+
+        $i = Instance::build();
+        $i->resolveNotify($notify);
     }
 }
